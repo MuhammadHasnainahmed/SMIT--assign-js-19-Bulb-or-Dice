1aa080f3e8d0711ae298e159d5eca66edce33699
Hasnain <97222458+MuhammadHasnainahmed@users.noreply.github.com>
2025-05-30 12:13:46 +0500

    all done

---
+++ app.js
@@ -0,0 +1,21 @@
+// let bulbon = document.getElementById("On").addEventListener("click" , function() {
+    
+//   document.getElementById('bulb').src='pic_bulbon.gif'
+    
+// } )
+
+// let bulboff = document.getElementById("off").addEventListener("click" , function() {
+//      document.getElementById('bulb').src='pic_bulboff.gif'
+
+// } )
+
+// let  = [1,2,3,4,5,6]
+
+let num = ["1" , "2" , "3" , "4" ,"5" ,"6"]
+function dice() {
+         let ramdon = Math.floor( Math.random() * num.length)+1
+        
+         document.getElementById('image').src=`${ramdon}.png`
+
+
+}
